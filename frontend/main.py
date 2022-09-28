7ca14da2ff187de53c896cc6bfcba0a7d3f13070
Andrey Guryanov <andreyg56@yandex.ru>
2022-09-28 15:50:15 +0500

    skeleton frontend

---
+++ frontend/main.py
@@ -0,0 +1,13 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    print('Hello, world!')
+    logging.DEBUG('Started successfully')
+
+
+if __name__ == '__main__':
+    main()
